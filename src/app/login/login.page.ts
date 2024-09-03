6a961fcfe083acf374c685ff5387072b0e92463b
Fenixssj <ale.lagosp@duocuc.cl>
2024-09-03 01:02:03 -0400

    RegistroAPP 1.1: Agregando el login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  username!: string;
+  password!: string;
+  welcomeMessage: string = 'Bienvenido!';
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private toastController: ToastController 
+  ) {}
+  
+  validateLogin() {
+    if (this.username === 'Admin' && this.password === 'Admin') {  // ACA ESTA EL USUARIO CON LA CONTRASEÑA
+      let extras: NavigationExtras = {
+        state: {
+          user: this.username
+        }
+      };
+      this.toastMessage('Usuario y contraseña válidos', 'success');
+      this.router.navigate(['/home'], extras);
+    } else {
+      this.toastMessage('Usuario o contraseña incorrectos, inténtelo de nuevo.', 'danger');
+    }
   }
 
+  async toastMessage(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom',
+      color: color
+    });
+
+    toast.present();
+  }
 }
