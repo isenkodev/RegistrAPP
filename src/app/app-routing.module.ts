c79e0002c7dc4fdd3abbb6a4f61e2b0a08bb14be
isenkodev <germanhuertaalvial690@gmail.com>
2024-09-03 00:17:12 -0400

    Proyecto RegistrAPP 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'recuperar',
+    loadChildren: () => import('./recuperar/recuperar.module').then( m => m.RecuperarPageModule)
+  },
 ];
 
 @NgModule({
