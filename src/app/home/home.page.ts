20d4eb839b2c88f2dda3283f2db3e4ea14ae3a30
Alejandro Lagos P. <134658061+Fenixssj@users.noreply.github.com>
2024-11-14 13:49:23 -0300

    Agregando la Alerta de la asistencia
    Acá agregue la alerta para escanear el código QR en el HOME

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { LoginServiceService } from '../service/login.service.service';
 import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
-import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
@@ -13,45 +13,51 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class HomePage {
 
-  username: string = 'guest'; // Valor predeterminado
+  username: string = 'guest'; 
+  result: string = '';
 
   constructor(
     private router: Router,
     private alertController: AlertController,
-    private loginService: LoginServiceService,
-    
- 
+    private loginService: LoginServiceService
   ) {
-    // Verificamos si el nombre de usuario fue pasado a través de la navegación
     const state = this.router.getCurrentNavigation()?.extras?.state;
     if (state && state['email']) {
       console.log(`User: ${state['email']}`);
-      this.username = state['email']; // Asignamos el valor de 'user' al username
+      this.username = state['email'];
     }
   }
 
   // Método para cerrar sesión
   onLogout() {
-    this.loginService.logout(); 
-    this.router.navigate(['/login']); 
+    this.loginService.logout();
+    this.router.navigate(['/login']);
+  }
+
+  // Método para mostrar alerta de éxito
+  async showSuccessMessage() {
+    const alert = await this.alertController.create({
+      header: '¡Éxito!',
+      message: '¡Asistencia registrada con éxito!',
+      buttons: ['OK'],
+    });
+    await alert.present();
   }
 
   // Método para escanear el código QR
-  result: string = ''
-
   async scan(): Promise<void> {
     const result = await CapacitorBarcodeScanner.scanBarcode({
       hint: CapacitorBarcodeScannerTypeHint.ALL
     });
     this.result = result.ScanResult;
+    
+    if (this.result) {
+      this.showSuccessMessage();
+    }
   }
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin]
-  
   }
-  
-
 }
-
