b50f0324331e1046c6fa9c3d5eb2ee77e50bf61d
Fenixssj <ale.lagosp@duocuc.cl>
2024-12-08 23:45:18 -0300

    Modificando

--- src/app/service/login.service.service.ts
+++ src/app/service/login.service.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginServiceService {
-  private isLoggedIn = false; 
-
   constructor() {}
 
+  // Método para iniciar sesión
   login() {
-    this.isLoggedIn = true;
+    // Guardamos la sesión del usuario en localStorage
+    localStorage.setItem('userLoggedIn', 'true');
+    localStorage.setItem('userEmail', 'user@example.com'); // Guardamos el email o cualquier otro dato
   }
 
+  // Método para cerrar sesión
   logout() {
-    this.isLoggedIn = false;
+    // Eliminamos los datos de sesión
+    localStorage.removeItem('userLoggedIn');
+    localStorage.removeItem('userEmail');
   }
 
+  // Verificar si el usuario está autenticado
   isAuthenticated(): boolean {
-    return this.isLoggedIn; 
+    return localStorage.getItem('userLoggedIn') === 'true';
   }
 }
